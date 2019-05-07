5a2e3e845d0b10ed36f1da4561ac2ef55ffd7a8f
bianquini <iagobianquini@gmail.com>
2019-05-07 11:36:52 -0300

    Frontend: criando pagina login

--- backend-app/src/main/java/br/edu/ifsc/lab/domain/Orcamento.java
+++ backend-app/src/main/java/br/edu/ifsc/lab/domain/Orcamento.java
@@ -6,14 +6,11 @@ public class Orcamento {
 	private Produto produtoCliente;
 	private Tecnico tecnico;
 	private Float valor;
-	
+
 	public Orcamento() {
-		
+
 	}
-	
-	
-	
-	
+
 	public Orcamento(Integer id, Produto produtoCliente, Tecnico tecnico, Float valor) {
 		super();
 		this.id = id;
@@ -22,41 +19,36 @@ public class Orcamento {
 		this.valor = valor;
 	}
 
-
-
-
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Tecnico getTecnico() {
 		return tecnico;
 	}
+
 	public void setTecnico(Tecnico tecnico) {
 		this.tecnico = tecnico;
 	}
+
 	public Float getValor() {
 		return valor;
 	}
+
 	public void setValor(Float valor) {
 		this.valor = valor;
 	}
-
-
-
 
 	public Produto getProdutoCliente() {
 		return produtoCliente;
 	}
 
-
-
-
 	public void setProdutoCliente(Produto produtoCliente) {
 		this.produtoCliente = produtoCliente;
 	}
-	
-	
+
 }
