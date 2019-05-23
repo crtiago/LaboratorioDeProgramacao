6bbe79a016f977783581e0130d2dd082d3e0f4d3
tiagocr0 <tiago.cr@aluno.ifsc.edu.br>
2019-05-23 17:38:06 -0300

    Backend: Deletando uma categoria com DELETE,e classes com erros personalizados criadas

--- backend-app/src/main/java/br/edu/ifsc/lab/resources/CategoriaResource.java
+++ backend-app/src/main/java/br/edu/ifsc/lab/resources/CategoriaResource.java
@@ -34,11 +34,11 @@ public class CategoriaResource {
 	 * informações de uma resposta http para o serviço REST,coloca uma interrogação
 	 * no tipo para dizer que pode ser qualquer tipo
 	 */
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) throws ObjectNotFoundException {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	/*
 	 * Método pra receber uma categoria no formato de Jsom e inserir essa categoria
 	 * no banco de dados
@@ -50,19 +50,29 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	/*
 	 * Método pra atualizar uma categoria no banco de dados
 	 */
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) throws ObjectNotFoundException {
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id)
+			throws ObjectNotFoundException {
 		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
-		
+
 		return ResponseEntity.noContent().build();
 	}
 
-	
+	/*
+	 * Método para deletar uma categoria do BD
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- backend-app/src/main/java/br/edu/ifsc/lab/services/CategoriaService.java
+++ backend-app/src/main/java/br/edu/ifsc/lab/services/CategoriaService.java
@@ -3,12 +3,14 @@ package br.edu.ifsc.lab.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifsc.lab.domain.Categoria;
 import br.edu.ifsc.lab.dto.CategoriaDTO;
 import br.edu.ifsc.lab.repository.CategoriaRepository;
-import javassist.tools.rmi.ObjectNotFoundException;
+import br.edu.ifsc.lab.services.exceptions.DataIntegrityException;
+import br.edu.ifsc.lab.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -25,7 +27,7 @@ public class CategoriaService {
 	/*
 	 * Criar uma operação capaz de buscar uma categoria por código
 	 */
-	public Categoria find(Integer id) throws ObjectNotFoundException {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = rep.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"" + "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
@@ -39,16 +41,15 @@ public class CategoriaService {
 		obj.setId(null);
 		return rep.save(obj);
 	}
-	
+
 	/*
 	 * Método para atualizar uma categoria
 	 */
 
-	public Categoria update(Categoria obj) throws ObjectNotFoundException {
+	public Categoria update(Categoria obj){
 		find(obj.getId());
 		return rep.save(obj);
 	}
-
 
 	/*
 	 * Método auxiliar,para que a partir de um objeto CategoriaDTO vai ser
@@ -56,6 +57,19 @@ public class CategoriaService {
 	 */
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+
+	/*
+	 * Método para deletar uma categoria
+	 */
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			rep.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
